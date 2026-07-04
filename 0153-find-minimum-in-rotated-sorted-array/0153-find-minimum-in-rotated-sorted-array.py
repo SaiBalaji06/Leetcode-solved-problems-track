db5f3de324ed96c5f25432a9080cb19accd4bb5c
SaiBalaji06 <124456452+SaiBalaji06@users.noreply.github.com>
2026-07-05 00:19:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (92.31%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,28 +4,28 @@ class Solution:
         high = len(nums) - 1
         mi = float("inf")
 
-        # while(high >= low):
-        #     mid = low + (high - low) // 2
-        #     if nums[high] > nums[low] and nums[mid] > nums[low]:
-        #         mi = min(mi, nums[low])
-        #         high = mid - 1
-        #     elif nums[low] > nums[high] and nums[mid] > nums[high]:
-        #         mi = min(mi, nums[high])
-        #         low = mid + 1
-        #     else:
-        #         mi = min(mi, nums[mid])
-        #         high = mid - 1
-        
-        # return mi
-
-        while(high > low):
+        while(high >= low):
             mid = low + (high - low) // 2
-            if nums[mid] > nums[high]:
+            if nums[high] > nums[low] and nums[mid] > nums[low]:
+                mi = min(mi, nums[low])
+                high = mid - 1
+            elif nums[low] > nums[high] and nums[mid] > nums[high]:
+                mi = min(mi, nums[high])
                 low = mid + 1
             else:
-                high = mid
+                mi = min(mi, nums[mid])
+                high = mid - 1
         
-        return nums[low]
+        return mi
+
+        # while(high > low):
+        #     mid = low + (high - low) // 2
+        #     if nums[mid] > nums[high]:
+        #         low = mid + 1
+        #     else:
+        #         high = mid
+        
+        # return nums[low]
             
 
 
